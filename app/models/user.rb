ada8306cba3c904d3bdb5b75dfa4ddb9c927f4a6
Masaki Komagata <komagata@gmail.com>
2016-07-14 15:50:36 +0900

    :sparkles: チームを追加
    アカウント登録時に自分がオーナーのチームを自動的に作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  has_many :affiliations
+  has_many :teams, through: :affiliations
+  has_one :my_affiliation, -> { where(ownership: true) }, class_name: 'Affiliation'
+  has_one :my_team, through: :my_affiliation, source: :team
+  after_create UserCallbacks.new
+
+  def create_my_team
+    transaction do
+      unless my_team
+        team = Team.create!
+        Affiliation.create!(
+          user: self,
+          team: team,
+          ownership: true
+        )
+      end
+    end
+  end
 end
