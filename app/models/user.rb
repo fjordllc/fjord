dd8127010abfc21f6311ebe1ddc7a991275d6d72
Masaki Komagata <komagata@gmail.com>
2016-07-25 16:24:13 +0900

    :sparkles: 招待機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :invitable
 
   has_many :affiliations
   has_many :teams, through: :affiliations
   has_one :my_affiliation, -> { where(ownership: true) }, class_name: 'Affiliation'
   has_one :my_team, through: :my_affiliation, source: :team
+
   after_create UserCallbacks.new
 
   def create_my_team
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714053750) do
+ActiveRecord::Schema.define(version: 20160725070029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160714053750) do
     t.boolean "ownership", default: false, null: false
     t.index ["team_id"], name: "index_affiliations_on_team_id", using: :btree
     t.index ["user_id"], name: "index_affiliations_on_user_id", using: :btree
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_projects_on_team_id", using: :btree
   end
 
   create_table "teams", force: :cascade do |t|
@@ -43,11 +51,23 @@ ActiveRecord::Schema.define(version: 20160714053750) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.string   "invited_by_type"
+    t.integer  "invited_by_id"
+    t.integer  "invitations_count",      default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "affiliations", "teams"
   add_foreign_key "affiliations", "users"
+  add_foreign_key "projects", "teams"
 end
