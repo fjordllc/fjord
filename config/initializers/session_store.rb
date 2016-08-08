691dc6b168ceecaa533e0d3fe84b41d43dcaa915
Masaki Komagata <komagata@gmail.com>
2016-08-08 20:55:16 +0900

    :tada: プロジェクトのCRUDを追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store, key: '_fjord_session', domain: {
-  'production'  => '.fjord.cm',
-  'staging'     => '.fjord-cm-dev.com'
+  'production' => '.fjord.cm',
+  'staging'    => '.fjord-cm-dev.com'
 }.fetch(Rails.env, '.lvh.me')
