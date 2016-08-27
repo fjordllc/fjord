d93ab182cebc0e6ee76d2f1e0d961d7017d847b1
Masaki Komagata <komagata@gmail.com>
2016-08-27 20:15:35 +0900

    reportのmarkdownを今日のデータだけに限定

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,8 +2,8 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @timelines = timelines(current_user)
-    @notes     = notes(current_user)
+    @timelines = timelines(current_user, today)
+    @notes     = notes(current_user, today)
 
     stub_time = Time.new(2000, 1, 1, 0, 0, 0)
     @start_time = @timelines&.first&.time || stub_time
