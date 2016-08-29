04d5ce1bbce0ddbef3f5eff68a08c5ba6d34914a
Masaki Komagata <komagata@gmail.com>
2016-08-29 15:29:58 +0900

    sucker_punchの設定追加

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Fjord
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
+    config.active_job.queue_adapter = :sucker_punch
   end
 end
